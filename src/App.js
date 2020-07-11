3169739500ffdaae8bf98779b775c22717be5c29
Antonio Samardzic <samardzic.antonio98@gmail.com>
2020-07-11 19:33:18 +0200

    All done w/o bonus

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-import DatePicker from "react-datepicker"
-import "react-datepicker/dist/react-datepicker.css"
 
 function App() {
   const [image, setImage] = useState("")
-  const [date, setDate] = useState(new Date())
-
-  const handleCalendarClose = () => console.log("Calendar closed")
-  const handleCalendarOpen = () => console.log("Calendar opened")
+  const [input, setInput] = useState({ date: "" })
 
   useEffect(() => {
     axios
@@ -18,22 +13,53 @@ function App() {
         `https://api.nasa.gov/planetary/apod?api_key=ogdSo3reoQL3v1GcXYatFWbmFOxqJgFJFnfbu4g3`
       )
       .then((res) => {
-        setImage(res.data.url)
+        setImage(res.data)
       })
   }, [])
 
+  let today = new Date()
+  let D = String(today.getDate()).padStart(2, "0")
+  let M = String(today.getMonth() + 1).padStart(2, "0")
+  let Y = today.getFullYear()
+  today = Y + "/" + M + "/" + D
+
+  const handleShowButton = (e) => {
+    e.preventDefault()
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=ogdSo3reoQL3v1GcXYatFWbmFOxqJgFJFnfbu4g3&date=${input.date}`
+      )
+      .then((res) => {
+        console.log(res.data)
+        setImage(res.data)
+        document.getElementsByClassName("image").src = input.image
+      })
+  }
+
   return (
-    <div className="App">
+    <div className="container ">
       <div>
-        <img className="image" src={image} alt="apod" />
+        <h1 className="mb-2 text-white text-3xl text-center">
+          Todays date: {today}
+        </h1>
+        <img className="images rounded" src={image.url} alt="APODImage" />
       </div>
-      <div>
-        <DatePicker
-          selected={date}
-          onChange={(date) => setDate(date)}
-          onCalendarClose={handleCalendarClose}
-          onCalendarOpen={handleCalendarOpen}
-        />
+
+      <div className="">
+        <input
+          className="rounded p-2 color-black font-bold text-center"
+          type="date"
+          id="date"
+          name="trip-start"
+          value={input.date}
+          onChange={(e) => setInput({ ...input, date: e.target.value })}
+        ></input>
+        <button
+          className="ml-2 mt-2 bg-white rounded p-2 color-black font-bold text-center"
+          onClick={handleShowButton}
+        >
+          Show Picture
+        </button>
       </div>
     </div>
   )
